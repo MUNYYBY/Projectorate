0bc6559d72a8fc7812de1f88181d8b40384af401
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-07 03:16:14 +0500

    Working on Login Page

--- pages/auth.js
+++ pages/auth.js
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import BG_IMAGE from "../public/Assets/login_bg_image.png";
+
 export default function Auth() {
-  return <div className="text-cyan-700">This is Auth page</div>;
+  return (
+    <div className="h-screen bg-primary">
+      <div className="secondry-section absolute bottom-0">
+        <Image
+          src={BG_IMAGE}
+          alt="Projectorate welcome image bottom-0"
+          sizes="cover"
+        />
+      </div>
+      <div className="primary-section">Hello world</div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5865F2",
+        secondry: "3BA55D",
+        gray: {
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e3e5e8",
+          200: "#ebedef",
+          100: "#f2f3f5",
+        },
+      },
+      spacing: {
+        88: "22rem",
+      },
+    },
   },
   plugins: [],
 };
