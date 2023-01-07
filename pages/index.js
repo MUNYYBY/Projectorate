d151ccfff895c8edd40e8e080d2ea4f5291777ba
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-07 06:02:24 +0500

    Customized landing page for under construction slogan

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
+import Working from "../public/Assets/working.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="">Hello world!</h1>;
+  return (
+    <div className="w-screen h-screen bg-primary flex flex-col justify-center items-center">
+      <div>
+        <Image
+          src={Working}
+          alt="projectorate under construction"
+          width={400}
+          height={400}
+        />
+      </div>
+      <div className="w-2/4 flex flex-col justify-center items-center">
+        <h1 className="font-demo text-4xl mb-2">Under Construction</h1>
+        <h2 className="text-2xl text-center">
+          We are working day and night to make this project a sucess. Kindly be
+          Patient!
+        </h2>
+      </div>
+      <div>
+        <Link href="/auth">
+          <button
+            type="button"
+            className="bg-secondry px-5 py-3 rounded-lg border-none my-5"
+          >
+            Go to Authentication Page
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
