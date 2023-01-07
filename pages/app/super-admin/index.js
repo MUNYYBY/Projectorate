ff1b40d05b293b8749ab4ed8d24ffd883a013c26
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-08 04:35:17 +0500

    Fixes

--- pages/app/super-admin/index.js
+++ pages/app/super-admin/index.js
@@ -3,6 +3,7 @@ import SubPanel from "../../../components/Sub-panel/SubPanel";
 import { SlSocialSteam } from "react-icons/sl";
 import { IoIosAdd, IoIosHelpCircle } from "react-icons/io";
 import { CgInbox } from "react-icons/cg";
+import { Tooltip } from "antd";
 
 export default function SuperAdminDashboard({ children }) {
   return (
@@ -13,20 +14,26 @@ export default function SuperAdminDashboard({ children }) {
         <header className="flex flex-row items-center justify-between border-b-2 border-b-gray-900 px-4 py-4">
           <div className="flex flex-row items-center">
             <div className="project-panel-icon mr-3">
-              <SlSocialSteam size={32} />
+              <SlSocialSteam size={30} />
             </div>
-            <h1 className="text-xl font-bold">Project Panel</h1>
+            <h1 className="text-lg font-bold">Project Panel</h1>
           </div>
           <div className="flex flex-row items-center">
-            <button className="bg-secondry py-1 px-3 rounded-md flex flex-row justify-center items-center">
-              <IoIosAdd size={26} />
-              <p>Create Project</p>
-            </button>
+            <Tooltip
+              placement="bottom"
+              title="Create a new project in your company and add a workforce"
+              mouseEnterDelay={0.05}
+            >
+              <button className="bg-secondry py-1 px-3 rounded-md flex flex-row justify-center items-center">
+                <IoIosAdd size={26} />
+                <p>Create Project</p>
+              </button>
+            </Tooltip>
             <div className="help-icon px-2">
-              <CgInbox size={32} />
+              <CgInbox size={28} />
             </div>
             <div className="inbox-icon px-2">
-              <IoIosHelpCircle size={32} />
+              <IoIosHelpCircle size={30} />
             </div>
           </div>
         </header>
