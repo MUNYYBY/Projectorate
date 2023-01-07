e1ca61f472f5c81d41d61066b5430dac05d93cae
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-07 06:11:19 +0500

    Added animated vector image to the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
 import Working from "../public/Assets/working.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-primary flex flex-col justify-center items-center">
       <div>
-        <Image
+        {/* <Image
           src={Working}
           alt="projectorate under construction"
           width={400}
           height={400}
-        />
+        /> */}
+        <Player
+          autoplay
+          loop
+          src="https://assets6.lottiefiles.com/private_files/lf30_tfozcvfo.json"
+          style={{ height: "300px", width: "300px" }}
+        ></Player>
       </div>
       <div className="w-2/4 flex flex-col justify-center items-center">
         <h1 className="font-demo text-4xl mb-2">Under Construction</h1>
