21da8457d07ce95395af1c23529008050636b7dc
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-08 23:46:39 +0500

    Working on Add Employees

--- pages/app/super-admin/index.js
+++ pages/app/super-admin/index.js
@@ -1,10 +1,14 @@
 import SideBar from "../../../components/SideBar/SideBar";
 import SubPanel from "../../../components/Sub-panel/SubPanel";
+import { useState, useEffect } from "react";
+import AddEmployee from "../../../components/AddEmployee/AddEmployee";
 export default function SuperAdminDashboard({ children }) {
+  const [addEmployees, setAddEmployees] = useState(false);
   return (
     <div className="Dashboard">
       <SideBar />
       <SubPanel />
+      <AddEmployee />
       {children}
     </div>
   );
