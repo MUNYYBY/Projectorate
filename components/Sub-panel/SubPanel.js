53dc44ce143a224ae4bbb4d6c1224b332f234bef
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-09 01:17:59 +0500

    Added Dyanamic panel name change based on routes change

--- components/Sub-panel/SubPanel.js
+++ components/Sub-panel/SubPanel.js
@@ -1,16 +1,27 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import { FiHome } from "react-icons/fi";
 import { AiOutlineFileDone } from "react-icons/ai";
 import { RxLapTimer } from "react-icons/rx";
 import ProjectTab from "./ProjectsTabs/ProjectTab";
 import TabDevider from "../Devider/Devider";
 import ProjectsTabContainer from "./ProjectsTabs/ProjectTabsContainer";
-import Link from "next/link";
+import { useRouteCheck } from "../../context/routesContext";
 
 export default function SubPanel() {
+  const { route } = useRouteCheck();
+  const [panelName, setPanelName] = useState("");
+
+  useEffect(() => {
+    if (route?.includes("projects")) setPanelName("Projects");
+    else if (route?.includes("employees")) setPanelName("Employees");
+    else if (route?.includes("teams")) setPanelName("Teams");
+    else setPanelName("Home");
+  }, [route]);
   return (
     <div className="Sub-panel-dahboard fixed ml-20 w-72 h-screen bg-gray-800">
       <div className="bg-gray-700 p-4 rounded-b-xl">
-        <h1 className=" font-semibold mb-2">Projects Panel</h1>
+        <h1 className=" font-semibold mb-2">{panelName} Panel</h1>
         <input
           className="h-8 w-full bg-gray-900 rounded-sm text-xs border-black px-3"
           placeholder="Search from all the projects..."
