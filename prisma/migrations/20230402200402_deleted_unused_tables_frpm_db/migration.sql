b6d6af3b70235f36ac4c9fc1f0ad7de92abea429
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-04-03 01:04:28 +0500

    Deleted unused tables from db

---
+++ prisma/migrations/20230402200402_deleted_unused_tables_frpm_db/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `admin` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `superadmin` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `admin`;
+
+-- DropTable
+DROP TABLE `superadmin`;
